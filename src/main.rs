43b5dec611b1868a336b1a8e3203dba16201eae7
hmidassi <hedy.midassi@gmail.com>
2023-11-03 00:42:24 +0100

    Adding initialization with Clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use clap::Command;
+
 fn main() {
-    println!("{:?}",std::env::args());
+    let _matches = Command::new("echor")
+        .version("0.1.0")
+        .author("Hedy Midassi")
+        .about("Echo made with Rust")
+        .get_matches();
+
 }
