2604a82653b87a81623740201d1efd5dfa433a00
hmidassi <hedy.midassi@gmail.com>
2023-11-03 13:14:29 +0100

    first arg ok

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
-use clap::Command;
+use clap::{Command, Arg};
 
 fn main() {
     let _matches = Command::new("echor")
         .version("0.1.0")
         .author("Hedy Midassi")
         .about("Echo made with Rust")
+        .arg(Arg::new("text")
+            .value_name("TEXT")
+            .help("Input text")
+            .required(true)
+            .num_args(1..))
         .get_matches();
 
 }
