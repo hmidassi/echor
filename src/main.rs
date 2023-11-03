5d1cb28059d81d471dcf24c7632fb6377ce4ff0d
hmidassi <hedy.midassi@gmail.com>
2023-11-03 13:51:36 +0100

    basic working code

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,37 @@
-use clap::{Command, Arg};
+use clap::{Arg, Command};
 
 fn main() {
-    let _matches = Command::new("echor")
+    let matches = Command::new("echor")
         .version("0.1.0")
         .author("Hedy Midassi")
         .about("Echo made with Rust")
-        .arg(Arg::new("text")
-            .value_name("TEXT")
-            .help("Input text")
-            .required(true)
-            .num_args(1..))
-        .arg(Arg::new("omit_newline")
-            .short('n')
-            .help("Do not print newline")
-            .num_args(0))
+        .arg(
+            Arg::new("text")
+                .value_name("TEXT")
+                .help("Input text")
+                .required(true)
+                .num_args(1..),
+        )
+        .arg(
+            Arg::new("omit_newline")
+                .short('n')
+                .help("Do not print newline")
+                .num_args(0),
+        )
         .get_matches();
 
-    println!("{:?}", _matches);
+    let text: Vec<&str> = matches
+        .get_many("text")
+        .unwrap()
+        .map(String::as_str)
+        .collect();
 
+    let omit_newline:bool = *matches.get_one("omit_newline").unwrap();
+
+    let mut ending = "\n";
+    if omit_newline {
+        ending = " ";
+    }
+
+    print!("{}{}", text.join(" "), ending);
 }
